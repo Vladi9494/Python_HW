b31ee6ce8cf831c68ad3bab8270a343dd636e227
Vladi9494 <v67870995@gmail.com>
2025-09-12 22:46:13 +0300

    Update test_02_calc.py

--- 06K_lesson/test_02_calc.py
+++ 06K_lesson/test_02_calc.py
@@ -10,8 +10,7 @@
 #                       =
 # 4. Проверьте (assert), что в окне отобразится результат 15 через 45 секунд.
 
-# import pytest
-from time import sleep
+
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.service import Service as ChromeService
@@ -20,59 +19,36 @@ from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
-driver = webdriver.Chrome(service=ChromeService(
-    ChromeDriverManager().install()))
 
-# При обрезке и переносе длинной строки ссылки,
-# на другую строку ссылка перестаёт работать ↓↓↓ (ошибка flake8)
-chrome_driver_path = r"C:\Users\0\Desktop\ПИТОН_ДОМАШКИ\ДОМАШКИ_по_PYTHON\Python_HW\05_lesson\chromedriver.exe"
+def test_calculator():
+    driver = webdriver.Chrome(service=ChromeService(
+        ChromeDriverManager().install()))
+    driver.maximize_window()
 
-driver = webdriver.Chrome(service=ChromeService(chrome_driver_path))
+    driver.get(
+       "https://bonigarcia.dev/selenium-webdriver-java/slow-calculator.html")
 
-# @pytest.fixture
-# def test_calculator():
-driver.maximize_window()
+    print("Сайт 'Slow calculator' открыт")
+    fild_delay = driver.find_element(By.CSS_SELECTOR, "#delay")
+    fild_delay.clear()
+    print("Поле 'delay' найдено и очищено от предыдущих записей")
+    fild_delay.send_keys("45")
+    print("B поле'delay'введено значение 45")
 
-driver.get(
-    "https://bonigarcia.dev/selenium-webdriver-java/slow-calculator.html")
+    driver.find_element(By.XPATH, "//*[@class='keys']/span[1]").click()
+    print("Кнопка '7' найдена и нажата")
+    driver.find_element(By.XPATH, "//*[@class='keys']/span[4]").click()
+    print("Кнопка '+' найдена и нажата")
+    driver.find_element(By.XPATH, "//*[@class='keys']/span[2]").click()
+    print("Кнопка '8' найдена и нажата")
+    driver.find_element(By.XPATH, "//*[@class='keys']/span[15]").click()
+    print("Кнопка '=' найдена и нажата")
 
-print("Сайт 'Slow calculator' открыт")
-sleep(1)
+    WebDriverWait(driver, 45).until(EC.text_to_be_present_in_element((
+        By.CSS_SELECTOR, "[class = 'screen']"), '15'))
+    res = driver.find_element(By.CSS_SELECTOR, "[class = 'screen']").text
+    assert res == "15"
+    print("сумма = " + res)
 
-fild_delay = driver.find_element(By.CSS_SELECTOR, "#delay")
-fild_delay.clear()
-print("Поле 'delay' найдено и очищено от предыдущих записей")
-sleep(1)
+    driver.quit()
 
-fild_delay.send_keys("45")
-print("B поле'delay'введено значение 45")
-sleep(1)
-
-# Определение локаторов для кнопок калькулятора:
-# НАСТАВНИК!!! почему данные локаторы работают,
-# но не определяются в консоли DevTools? ("is not a valid selector") ↓↓↓
-# $$ ("//span[text()= '7']")
-# $$ ("//*[@id='calculator']/div[2]/span[1]")
-# $$ ("//*[@class='keys']/span[1]")
-
-driver.find_element(By.XPATH, "//*[@class='keys']/span[1]").click()
-sleep(1)
-print("Кнопка '7' найдена и нажата")
-driver.find_element(By.XPATH, "//*[@class='keys']/span[4]").click()
-sleep(1)
-print("Кнопка '+' найдена и нажата")
-driver.find_element(By.XPATH, "//*[@class='keys']/span[2]").click()
-sleep(1)
-print("Кнопка '8' найдена и нажата")
-driver.find_element(By.XPATH, "//*[@class='keys']/span[15]").click()
-sleep(1)
-print("Кнопка '=' найдена и нажата")
-
-# $$("[class = 'screen']")
-WebDriverWait(driver, 45).until(EC.text_to_be_present_in_element((
-    By.CSS_SELECTOR, "[class = 'screen']"), '15'))
-res = driver.find_element(By.CSS_SELECTOR, "[class = 'screen']").text
-assert res == "15"
-print("сумма = " + res)
-
-driver.quit()
