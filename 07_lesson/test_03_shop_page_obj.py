330ad7858bae4cc664d92a25206698a47edd1315
Vladi9494 <v67870995@gmail.com>
2025-09-20 12:03:36 +0300

    Update test_03_shop_page_obj.py

--- 07_lesson/test_03_shop_page_obj.py
+++ 07_lesson/test_03_shop_page_obj.py
@@ -18,16 +18,14 @@
 # Проверить (assert), что итоговая сумма равна $58.29.
 
 from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.firefox.service import Service as FirefoxService
 import pytest
-from time import sleep
 from selenium import webdriver
 from Autorization import Autorization
 from MainPage import MainPage
 from CartPage import CartPage
 from Check_Out_Page import CheckoutPage
+
 
 @pytest.fixture
 def driver():
@@ -41,52 +39,28 @@ def driver():
     yield driver
     driver.quit()
 
-    sleep(5)
-    # Авторизоваться как пользователь standard_user.
-def test_autorization(driver):
+
+def test_shop_flow(driver):
     auth_page = Autorization(driver)
     auth_page.input_login()
-    # Ожидание загрузки страницы с товарами
-    WebDriverWait(driver, 5).until(
-        EC.presence_of_element_located((By.CLASS_NAME, "inventory_item")))
-    sleep(5)
     # Добавление в корзину товаров
-def test_main_page(driver):
     main_page = MainPage(driver)
     main_page.add_to_cart()
+    # Проверка товаров в корзине
+    cart_badge = driver.find_element(By.CLASS_NAME, "shopping_cart_badge")
+    assert cart_badge.text == "3", "В корзине должно быть 3 товара"
     # Переход в корзину
     main_page.go_to_cart()
-    # Ожидание загрузки страницы корзины
-    WebDriverWait(driver, 5).until(
-            EC.presence_of_element_located((By.CLASS_NAME, "cart_item")))
-    sleep(4)    
-#     # Нажать кнопку Checkout
-# def test_cart_page(driver):
-#     cart = CartPage(driver)
-#     cart.click_checkout() 
-#     cart.cart_contents()
-#     actual_content = cart.cart_contents()
-#     expected_content = "Sauce Labs Backpack\nSauce Labs Bolt T-Shirt\nSauce Labs Onesie"    
-#     assert actual_content == expected_content
- 
-# cart_content = cart_page.get_cart_content()
-# assert cart_content == "Ожидаемое содержание"
-
-
-       # Заполненение формы своими данными (Имя, Фамилия, Почтовый индекс)
-# def test_check_out_page(driver):
-#     personal_data = CheckoutPage(browser)
-#     personal_data.input_info() 
-#     personal_data.check_prise()   
-
-   
+    # Нажать кнопку Checkout
+    cart = CartPage(driver)
+    cart.click_checkout()
+    # Заполненение формы своими данными (Имя, Фамилия, Почтовый индекс)
+    personal_data = CheckoutPage(driver)
+    personal_data.input_info()
+    personal_data.check_prise()
     # Прочитать со страницы итоговую стоимость (Total)
-    # total_price = buy.check_cost()
-    # expected = "Total: $58.29"
-    # assert expected == "total_price"
-
-    # Закрыть браузер
-    # self.driver.quit()
-    # Проверить (assert), что итоговая сумма равна $58.29.
-
-
+    text_prise = driver.find_element(
+        By.CSS_SELECTOR, "div.summary_total_label").text
+    text_prise_value = float(text_prise.split("$")[1])
+    driver.quit()
+    assert text_prise_value == 58.29
