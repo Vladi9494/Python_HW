20ce6fe6c3b5b1b5197c3b715807d88d3d24a30a
Vladi9494 <v67870995@gmail.com>
2025-09-20 16:18:28 +0300

    Update Check_Out_Page.py

--- 07_lesson/Check_Out_Page.py
+++ 07_lesson/Check_Out_Page.py
@@ -1,23 +1,3 @@
-# Автотест на интернет-магазин
-# Цель: написать автотест для проверки функциональности
-# интернет-магазина на сайте https://www.saucedemo.com/,
-# используя паттерн Page Object.
-# Шаги:
-# Создание класса Page Object:
-
-# Создать класс для страницы авторизации, который будет содержать методы
-# для ввода логина и пароля, а также для нажатия кнопки входа.
-
-# Создать класс для главной страницы магазина, который будет содержать
-# методы для добавления товаров в корзину и перехода в корзину.
-
-# Создать класс для страницы корзины, который будет содержать методы
-# для нажатия кнопки Checkout и проверки содержимого корзины.
-
-# Создать класс для страницы оформления заказа, который будет содержать
-# методы для заполнения формы данными (имя, фамилия, почтовый индекс)
-# и проверки итоговой стоимости.
-
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
@@ -26,6 +6,8 @@ from selenium.webdriver.support import expected_conditions as EC
 # Создать класс для страницы оформления заказа, который будет содержать
 # методы для заполнения формы данными (имя, фамилия, почтовый индекс)
 # и проверки итоговой стоимости.
+
+
 class CheckoutPage:
     def __init__(self, driver):
         self.driver = driver
@@ -43,15 +25,26 @@ class CheckoutPage:
 
         # Проверка кликабельности кнопки "Сontinue"
         contin = WebDriverWait(self.driver, 20).until(
-            EC.element_to_be_clickable((By.CSS_SELECTOR, "[id='continue']")))
-        contin = self.driver.find_element(By.CSS_SELECTOR, "[id='continue']")
+            EC.element_to_be_clickable((
+                By.CSS_SELECTOR, "[id='continue']")))
+        contin = self.driver.find_element(
+            By.CSS_SELECTOR, "[id='continue']")
         contin.click()
         print("Кнопка 'Continue' нажата")
         self.driver.execute_script(
           "window.scrollTo(0, document.body.scrollHeight);")  # прокрутка вниз
 
     def check_prise(self):
-        # Чтение итоговой стоимости
+        # Чтение итоговой стоимости        
+        text_prise = self.driver.find_element(
+            By.CSS_SELECTOR, "div.summary_total_label").text
+        text_prise_value = float(text_prise.split("$")[1])
+        text_prise_value == 58.29
         text_prise = self.driver.find_element(
             By.CSS_SELECTOR, "div.summary_total_label").text
         print(text_prise)
+
+    def get_total_price(self):
+        element = self.driver.find_element(
+            By.CSS_SELECTOR, "div.summary_total_label")
+        return float(element.text.split("$")[1])
