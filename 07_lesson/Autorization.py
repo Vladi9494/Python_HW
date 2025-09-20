11bafdfc51482cf735e034375151ca705d2c592a
Vladi9494 <v67870995@gmail.com>
2025-09-20 11:53:02 +0300

    Update Autorization.py

--- 07_lesson/Autorization.py
+++ 07_lesson/Autorization.py
@@ -1,20 +1,20 @@
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-from time import sleep
 
 # Создать класс для страницы авторизации, который будет содержать методы
 # для ввода логина и пароля, а также для нажатия кнопки входа.
+
+
 class Autorization:
     def __init__(self, driver):
-        self.driver = driver  
-        self.driver.get("https://www.saucedemo.com/")
+        self.driver = driver
 
-    sleep(6)
     def input_login(self):
         self.driver.find_element(By.ID, "user-name").send_keys("standard_user")
         self.driver.find_element(By.ID, "password").send_keys("secret_sauce")
         self.driver.find_element(By.ID, "login-button").click()
-    sleep(4)    
-        
-    
+        print("Кнопка 'Login' нажата")
+        # Ожидание загрузки страницы с товарами
+        WebDriverWait(self.driver, 5).until(
+            EC.presence_of_element_located((By.CLASS_NAME, "inventory_item")))
