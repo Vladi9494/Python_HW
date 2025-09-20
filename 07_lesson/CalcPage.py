7e3674cd98297528bd73b5e60363ec8214ac205d
Vladi9494 <v67870995@gmail.com>
2025-09-20 16:16:01 +0300

    Update CalcPage.py

--- 07_lesson/CalcPage.py
+++ 07_lesson/CalcPage.py
@@ -1,24 +1,3 @@
-# Общие требования:
-# Создайте классы с описанием страниц.
-# Каждый тест должен создавать и использовать
-# необходимый объект страницы (Page).
-# Каждый тест может только создавать, настраивать и закрывать драйвер.
-# Остальные selenium-методы (click, send_keys, text…)
-# запрещены в коде тестов (должны быть в классах страниц).
-# В классах страниц не должно быть проверок.
-
-# Автотест на калькулятор
-# Цель: написать автотест для проверки функциональности калькулятора на сайте
-# https://bonigarcia.dev/selenium-webdriver-java/slow-calculator.html,
-# используя паттерн Page Object.
-# Шаги:
-# Создание класса Page Object:
-# Создать класс для страницы калькулятора,
-# который будет содержать методы для взаимодействия с элементами:
-# Поле ввода задержки (локатор #delay).
-# Кнопки калькулятора (цифры, операторы, кнопка =).
-# Поле вывода результата.
-
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
@@ -30,8 +9,10 @@ class CalcPage:
         self.wait = WebDriverWait(driver, 6)
 
     def setting_waiting(self):
-        self.driver.find_element(By.CSS_SELECTOR, "#delay").clear()
-        self.driver.find_element(By.CSS_SELECTOR, "#delay").send_keys("45")
+        self.driver.find_element(
+            By.CSS_SELECTOR, "#delay").clear()
+        self.driver.find_element(
+            By.CSS_SELECTOR, "#delay").send_keys("45")
 
     def calculator_buttons(self):
         self.driver.find_element(
@@ -54,3 +35,4 @@ class CalcPage:
 
     def close_driver(self):
         self.driver.quit()
+
