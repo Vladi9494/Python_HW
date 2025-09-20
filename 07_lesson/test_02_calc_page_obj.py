43b6cbd17d42104a42e9c78b5828ae1e7193a927
Vladi9494 <v67870995@gmail.com>
2025-09-20 16:20:30 +0300

    Update test_02_calc_page_obj.py

--- 07_lesson/test_02_calc_page_obj.py
+++ 07_lesson/test_02_calc_page_obj.py
@@ -1,17 +1,3 @@
-# Каждый тест может только создавать, настраивать и закрывать драйвер.
-# Остальные selenium-методы (click, send_keys, text…)
-# запрещены в коде тестов (должны быть в классах страниц).
-# В классах страниц не должно быть проверок.
-
-# Создание теста:
-# Написать тест, который использует PageObject
-# для выполнения следующих действий:
-# Открыть страницу калькулятора.
-# Ввести значение 45 в поле задержки (локатор #delay).
-# Нажать кнопки: 7, +, 8, =.
-# Проверить (assert), что в окне отобразится
-# результат 15 через 45 секунд.
-
 from selenium.webdriver.common.by import By
 import pytest
 from selenium import webdriver
@@ -39,11 +25,13 @@ def test_calclator(driver):
     result = calc_page.result()
 
     # Поиск элемента и извлечние текста
-    result_element = driver.find_element(By.CSS_SELECTOR, "[class = 'screen']")
+    result_element = driver.find_element(
+        By.CSS_SELECTOR, "[class = 'screen']")
     result = result_element.text.strip()
 
     # Проверка результата
-    result_element = driver.find_element(By.CSS_SELECTOR, "[class = 'screen']")
+    result_element = driver.find_element(
+        By.CSS_SELECTOR, "[class = 'screen']")
     result = result_element.text.strip()
     assert result == "15"
 
