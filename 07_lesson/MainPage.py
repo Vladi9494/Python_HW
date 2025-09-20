ec2f193432ccbf4d31bf09f575c7f611f3f5bc6b
Vladi9494 <v67870995@gmail.com>
2025-09-20 12:00:21 +0300

    Update MainPage.py

--- 07_lesson/MainPage.py
+++ 07_lesson/MainPage.py
@@ -1,29 +1,43 @@
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-from time import sleep
+
 
 # Создать класс для главной страницы магазина, который будет содержать
 # методы для добавления товаров в корзину и перехода в корзину.
+
+
 class MainPage:
     def __init__(self, driver):
         self._driver = driver
-        self._driver.get("https://www.saucedemo.com/inventory.html")
 
     # добавление товаров в корзину
     def add_to_cart(self):
-        add_button = self._driver.find_element(By.ID, "add-to-cart-sauce-labs-backpack")
-        #add_button = self._driver.find_element(By.CSS_SELECTOR, 'button[data-test="add-to-cart-sauce-labs-backpack"]')
-        add_button.click()
-        add_button = self._driver.find_element(
+        # Ждем пока кнопка станет кликабельной
+        button = WebDriverWait(self._driver, 15).until(
+            EC.element_to_be_clickable((
+                By.ID, "add-to-cart-sauce-labs-backpack")))
+        button.click()
+        button = self._driver.find_element(
             By.ID, "add-to-cart-sauce-labs-bolt-t-shirt").click()
-        add_button = self._driver.execute_script("window.scrollTo(0, 500)") # прокрутка вниз
-        self._driver.find_element(By.ID, "add-to-cart-sauce-labs-onesie").click()
-    
-    sleep(6)
-    # переход в корзину
-    def go_to_cart(self):
-        self._driver.find_element(By.CSS_SELECTOR, "button[class='btn btn_primary btn_small btn_inventory ']").click()
+        button = self._driver.execute_script(
+            "window.scrollTo(0, 500)")  # прокрутка вниз
+        self._driver.find_element(
+            By.ID, "add-to-cart-sauce-labs-onesie").click()
+        button = self._driver.execute_script(
+            "window.scrollTo(500, 0)")  # прокрутка вверх
 
-    # ожидание загрузки страницы корзины
-   
+    def go_to_cart(self):
+        # Проверка кликабельности кнопки "Корзина"
+        basket = WebDriverWait(self._driver, 20).until(
+            EC.element_to_be_clickable((
+                By.CSS_SELECTOR, "a.shopping_cart_link")))
+        basket = self._driver.find_element(
+            By.CSS_SELECTOR, "a.shopping_cart_link")
+        basket.click()
+        print("Кнопка 'Корзина' нажата")
+        # ожидание загрузки страницы корзины
+        WebDriverWait(self._driver, 10).until(
+            EC.presence_of_element_located((By.CLASS_NAME, "cart_item")))
+        self._driver.execute_script(
+          "window.scrollTo(0, document.body.scrollHeight);")  # прокрутка вниз
