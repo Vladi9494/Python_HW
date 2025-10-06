4d1e8893e44460d8b600f87c77fe019abff2d756
Vladi9494 <v67870995@gmail.com>
2025-10-07 01:56:29 +0300

    Update test_03_shop_page_obj_allure.py

--- 10_lesson/test_03_shop_page_obj_allure.py
+++ 10_lesson/test_03_shop_page_obj_allure.py
@@ -11,7 +11,8 @@ import allure
 @pytest.fixture
 def driver():
     """
-    Фикстура для инициализации, открытия сайта магазина и завершения работы драйвера.
+    Фикстура для инициализации, открытия сайта магазина
+      и завершения работы драйвера.
     """
     geckodriver_path = (r"C:\Users\0\Desktop\ПИТОН_ДОМАШКИ\ДОМАШКИ_по_PYTHON"
                         r"\Python_HW\05_lesson\geckodriver.exe")
@@ -23,6 +24,7 @@ def driver():
     yield driver
     driver.quit()
 
+
 @allure.title("Тестирование сайта магазина")
 @allure.description("Тест проверяет корректность работы сайта магазина "
                     "с различными операциями по заказу товаров.")
@@ -33,30 +35,31 @@ def test_shop_flow(driver):
     with allure.step("Ввод данных для авторизации ('user-name'"
                      " 'password' и  нажатие кнопки 'login')"):
         auth_page.input_login()
-    
+
     main_page = MainPage(driver)
-    with allure.step("Добавление товаров в корзину"):   
+    with allure.step("Добавление товаров в корзину"):
         main_page.add_to_cart()
-    
+
     with allure.step("Проверка товаров в корзине"):
         cart_badge = main_page.cart_badge()
         assert cart_badge == "3", "В корзине должно быть 3 товара"
-    
+
     with allure.step("Переход в корзину"):
         main_page.go_to_cart()
-        
+
     cart = CartPage(driver)
     with allure.step("Нажать на кнопку 'checkout'"):
         cart.click_checkout()
-    
+
     personal_data = CheckoutPage(driver)
-    with allure.step("Заполнение формы (оформления заказа)"
-    " 'checkout' личными данными: Имя, Фамилия, Почтовый индекс"):
+    with allure.step("Заполнение формы (оформления заказа)'checkout'"
+                     " личными данными: Имя, Фамилия, Почтовый индекс"):
         personal_data.input_info()
         personal_data.check_prise()
-    
+
     checkout_page = CheckoutPage(driver)
-    with allure.step("Чтение со страницы итоговой стоимости заказа (total_price)"):
+    with allure.step("Чтение со страницы итоговой стоимости заказа"
+                     " (total_price)"):
         total_price = checkout_page.get_total_price()
     driver.quit()
 
